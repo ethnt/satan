f571fd2818ea757cab068f6bb373864f4dfa5e61
Ethan Turkeltaub <ethan@turkeltaub.dev>
2021-01-04 15:54:45 -0500

    Add easier way to log in to machines

--- machines/common.nix
+++ machines/common.nix
@@ -1,13 +1,17 @@
-{
+{ pkgs, ... }: {
   nix.gc = {
     automatic = true;
     dates = "03:15";
   };
 
-  programs.mosh.enable = true;
+  services.openssh.enable = true;
 
   services.zerotierone = {
     enable = true;
     joinNetworks = [ "e4da7455b2239e18" ];
   };
+
+  programs.mosh.enable = true;
+
+  environment.systemPackages = with pkgs; [ htop ];
 }
--- shell.nix
+++ shell.nix
@@ -2,13 +2,13 @@ let
   sources = import ./nix/sources.nix;
   nixpkgs = sources.nixpkgs;
   pkgs = import nixpkgs { };
+  PWD = "${builtins.getEnv "PWD"}";
 in pkgs.mkShell rec {
   name = "satan";
 
   buildInputs = with pkgs; [ git-crypt niv nixfmt morph ];
 
-  # shellHook = ''
-  #   export NIXOPS_STATE=./opt/deployments.nixops
-  #   export NIXOPS_DEPLOYMENT=satan
-  # '';
+  shellHook = ''
+    export SSH_IDENTITY_FILE="${PWD}/keys/satan"
+  '';
 }
