3ed59abbf07a1cc4f330b50e49e8d94c646249eb
Ethan Turkeltaub <ethan@turkeltaub.dev>
2021-01-28 01:08:17 -0500

    Get a function to make a config working

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ build-funnel:
 	nix build .#nixosConfigurations.funnel.config.system.build.toplevel
 
 deploy:
-	nix-shell --command "NIX_SSHOPTS=\"-i ./keys/satan\" nix run github:serokell/deploy-rs"
+	nix develop -c "nix" "run" "github:serokell/deploy-rs"
--- flake.nix
+++ flake.nix
@@ -8,12 +8,73 @@
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, deploy-rs }:
-    {
-      nixosConfigurations = {
-        funnel = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+    let
+      inherit (nixpkgs) lib;
+
+      # List of all used platforms/architectures
+      platforms = [ "x86_64-linux" "aarch64-linux" ];
+
+      # Helper to do a thing for all our platforms
+      forAllPlatforms = f: lib.genAttrs platforms (platform: f platform);
+
+      # Generates a nixpkgs configuration for all of our platforms
+      nixpkgsFor = forAllPlatforms (platform:
+        import nixpkgs {
+          system = platform;
+          config.allowUnfree = true;
+        });
+
+      # A function to generate a standard configuration. Arguments are:
+      #   - system: See a `platform` from above
+      #   - machine: The name of the machine
+      #   - machineConfiguration: Path to the configuration (optional, defaults to ./machines/${machine}/configuration.nix)
+      #   - extraModules: Extra modules to include (optional, defaults to empty list)
+      mkConfig = { system, machine
+        , machineConfiguration ? ./machines + "/${machine}/configuration.nix"
+        , extraModules ? [ ] }:
+        let
+          defaults = { pkgs, lib, ... }: {
+            imports = [ machineConfiguration ];
+
+            nix.gc = {
+              automatic = true;
+              dates = "03:15";
+            };
+
+            time.timeZone = "America/New_York";
+
+            networking.firewall = {
+              enable = true;
+              allowedTCPPorts = [ 22 ];
+              allowedUDPPorts = [ 22 ];
+              allowedUDPPortRanges = [{
+                from = 60000;
+                to = 61000;
+              }];
+            };
+
+            services.openssh.enable = true;
+
+            services.zerotierone = {
+              enable = true;
+              joinNetworks = [ "e4da7455b2239e18" ];
+            };
+
+            programs.mosh.enable = true;
+
+            environment.systemPackages = with pkgs; [ htop ];
+          };
+        in inputs.nixpkgs.lib.nixosSystem {
+          modules = [ defaults ] ++ extraModules;
+          system = system;
           specialArgs = { inherit inputs nixpkgs; };
-          modules = [ ./machines/funnel/configuration.nix ];
+          pkgs = nixpkgsFor.${system};
+        };
+    in {
+      nixosConfigurations = {
+        funnel = mkConfig {
+          system = "x86_64-linux";
+          machine = "funnel";
         };
       };
 
